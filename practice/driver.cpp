bee7e88efc5fed3ab65001fdf4346cfad649d1dd
Chao Ma <machao@seas.upenn.edu>
2013-02-27 20:21:32 -0500

    some practice about multi-thread

--- practice/driver.cpp
+++ practice/driver.cpp
@@ -4,6 +4,9 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include "unistd.h"
+#include "fcntl.h"
+#include "sys/wait.h"
 
 struct node {
   int value;
@@ -47,4 +50,35 @@ int main(int argc, char** argv) {
     free(pre);
   }
 
+
+  int fd = open("./testInput.txt", O_RDWR | O_CREAT, 0766);
+  //dup2(fd, 1);
+  
+  printf("%d\n", fd);
+  char chars[10];
+  int read_num = read(fd, chars, 9);
+  chars[9] = '\0';
+  
+  printf("%d\n%s\n", read_num, chars);
+
+  lseek(fd, -5, SEEK_CUR);
+  
+  write(fd, "this is newer", 14);
+
+  int *i = new int;
+  int pid = fork();
+  if(pid == 0) {
+    write(1, "Hello from child.\n", 18);
+    int result = execl("/bin/date", "date", NULL);
+    printf("%d\n", result);
+    delete i;
+    exit(0);
+  } else {
+    //waitpid(pid, i, 0);
+    wait(i);
+    write(1, "Hello form parent.\n", 19);
+    delete i;
+  } 
+
+  
 }
